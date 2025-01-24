6abbae16362da062cc1d20d94eb77fe20d34ba77
Mark Slorach <hello@markslorach.com>
2025-01-24 23:36:41 +0000

    Refactor Footer and ScrollButton components for improved responsiveness and styling

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -3,24 +3,46 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="px-4 md:container flex h-20 items-center justify-between mt-10">
-      <p>
-        created by{" "}
+    <footer className="px-4 border-t h-20 mt-20">
+      <div className="md:container flex h-20 items-center text-sm justify-between text-black/60">
+        <span className="hidden md:inline">
+          Built using the{" "}
+          <Link
+            className="text-blue-500"
+            href="https://restcountries.com/"
+            target="_blank"
+          >
+            REST Countries API
+          </Link>{" "}
+          by{" "}
+          <Link
+            className="text-blue-500 underline underline-offset-2"
+            href="https://www.markslorach.com/"
+            target="_blank"
+          >
+            Mark Slorach
+          </Link>
+          .
+        </span>
+
+        <span className="md:hidden">
+          Built by{" "}
+          <Link
+            className="text-blue-500 underline underline-offset-2"
+            href="https://www.markslorach.com/"
+            target="_blank"
+          >
+            Mark Slorach
+          </Link>
+          .
+        </span>
         <Link
-          className="text-blue-500"
-          href="https://www.markslorach.com/"
+          href="https://github.com/markslorach/countries-db.git"
           target="_blank"
         >
-          mark slorach
+          <Github className="h-5 w-5 text-black" />
         </Link>
-        .
-      </p>
-      {/* <Link
-        href="https://github.com/markslorach/countries-db.git"
-        target="_blank"
-      >
-        <Github className="h-5 w-5" />
-      </Link> */}
+      </div>
     </footer>
   );
 };
--- app/components/shared/ScrollButton.tsx
+++ app/components/shared/ScrollButton.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import { ChevronUp } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
 const ScrollButton = () => {
   const [visible, setVisible] = useState(false);
@@ -31,14 +30,14 @@ const ScrollButton = () => {
   }, []);
 
   return (
-    <Button
-      className={`shadow-sm rounded-full fixed sm:bottom-10 sm:right-10 bottom-4 right-4 p-0 h-10 w-10 ${
+    <button
+      className={`shadow-sm opacity-90 bg-black/60 sm:bg-black sm:opacity-50 sm:hover:opacity-70 transition-opacity duration-300 rounded-full fixed sm:bottom-24 sm:right-10 bottom-24 right-4 p-2.5 ${
         visible ? "" : "hidden"
       }`}
       onClick={scrollToTop}
     >
-      <ChevronUp />
-    </Button>
+      <ChevronUp className="text-white" />
+    </button>
   );
 };
 
